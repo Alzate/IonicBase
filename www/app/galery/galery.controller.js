b5af60782719c2b9f749e4ea9b95761b03a712a3
Eslem <j.eslem03@gmail.com>
2015-09-20 03:59:58 +0200

    linux backup

--- www/app/galery/galery.controller.js
+++ www/app/galery/galery.controller.js
@@ -1,12 +1,11 @@
-
 angular.module('starter.controllers')
 
 .controller('GaleryCtrl', function($scope, $ionicSlideBoxDelegate) {
-  $scope.images = [new ImageSlide(null, 'assets/img/skull.jpg'),new ImageSlide(null, 'assets/img/eye.jpg'), new ImageSlide(null, 'assets/img/angel.jpg'), new ImageSlide(null, 'assets/img/skyrim.jpg')];
+  $scope.images = [new ImageSlide(null, 'assets/img/product1.jpg'), new ImageSlide(null, 'assets/img/product2.jpg'), new ImageSlide(null, 'assets/img/product3.jpg')];
   $ionicSlideBoxDelegate.update();
 });
 
-var ImageSlide = function(title, path){
+var ImageSlide = function(title, path) {
   this.title = title;
   this.path = path;
 };
